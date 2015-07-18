9debcd286b0f9586a55bfe221871b1baa23cdbba
Pedro Olivares Sánchez <piotroli@gmail.com>
2015-07-18 12:20:56 +0200

    Despliegue base de datos en Heroku

--- models/models.js
+++ models/models.js
@@ -1,12 +1,30 @@
 var path = require('path');
 
+// PostgreSQL DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite     DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar base de datos SQLite
-var sequelize = new Sequelize(null, null, null, 
-                       {dialect: "sqlite", storage: "quiz.sqlite"}
-                    );
+// Usar base de datos SQLite o PostgreSQL
+var sequelize = new Sequelize(DB_name, user, pwd,
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo PostgreSQL
+  }
+);
 
 // Importar definición de tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
@@ -19,9 +37,9 @@ sequelize.sync().success(function() {
   Quiz.count().success(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta: 'Capital de Italia',
-      	            respuesta: 'Roma'
-      	         })
-      .success(function(){console.log('Base de datos inicializada')});
-    };
+                    respuesta: 'Roma'
+                  })
+      .success(function(){console.log('Base de datos inicializada');});
+    }
   });
 });
