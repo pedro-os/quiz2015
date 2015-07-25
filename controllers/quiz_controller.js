95324f7d843ddec997a049eaa4d74e06d9260fcc
Pedro Olivares Sánchez <piotroli@gmail.com>
2015-07-25 17:29:33 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -28,6 +28,23 @@ exports.index = function(req, res) {
     }
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // Crea objeto quiz
+      {pregunta: "Pregunta", respuesta: "Respuesta"}
+    );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+      res.redirect('/quizes');
+    });
+};
+
 // GET /quizes/:id
 exports.show = function(req, res) {
   res.render('quizes/show', { quiz: req.quiz});
